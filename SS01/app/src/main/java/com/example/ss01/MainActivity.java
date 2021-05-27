073f25ec18b34eaadabfdc7446d4fa5ab2fe4528
Stas Jng <sonthth1903012@fpt.edu.vn>
2021-05-28 00:47:59 +0700

    init

---
+++ SS01/app/src/main/java/com/example/ss01/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ss01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo02);
+    }
+
+
+}

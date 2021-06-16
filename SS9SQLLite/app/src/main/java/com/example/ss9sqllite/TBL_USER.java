cca65d1883ae57d9e3fa0a8206706ebc87a0d4a9
Stas Jng <sonthth1903012@fpt.edu.vn>
2021-06-16 09:48:43 +0700

    update

---
+++ SS9SQLLite/app/src/main/java/com/example/ss9sqllite/TBL_USER.java
@@ -0,0 +1,9 @@
+package com.example.ss9sqllite;
+
+public class TBL_USER {
+    public static String id="id";
+    public static String name="name";
+    public static String gender="gender";
+    public static String age="age";
+
+}

bed33fd6e93d418ca94c9f2ff83b4fec650fefaf
Stas Jng <sonthth1903012@fpt.edu.vn>
2021-06-18 06:26:55 +0700

    update

---
+++ SS9SQLLite_2/app/src/main/java/com/example/ss9sqllite_2/ListUserAct.java
@@ -0,0 +1,63 @@
+package com.example.ss9sqllite_2;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListUserAct extends AppCompatActivity {
+    private DBHelper db;
+    private Cursor c;
+    private SimpleCursorAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_user);
+
+        db = new DBHelper(this);
+        ListView lvUser = (ListView) findViewById(R.id.lvUser);
+
+        c = db.getAllUser();
+
+        adapter = new SimpleCursorAdapter(this, R.layout.item_user, c, new String[]{
+                DBHelper.ID, DBHelper.NAME, DBHelper.GENDER}, new int[]{R.id.tvId, R.id.tvName, R.id.tvGender}, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        lvUser.setAdapter(adapter);
+
+        lvUser.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Cursor cursor = (Cursor) adapter.getItem(position);
+                int _id = cursor.getInt(cursor.getColumnIndex(DBHelper.ID));
+                String name = cursor.getString(cursor.getColumnIndex(DBHelper.NAME));
+                String gender = cursor.getString(cursor.getColumnIndex(DBHelper.GENDER));
+                String des = cursor.getString(cursor.getColumnIndex(DBHelper.DES));
+
+                Intent intent = new Intent(ListUserAct.this, UpdateAct.class);
+                intent.putExtra(DBHelper.ID, _id);
+                intent.putExtra(DBHelper.NAME, name);
+                intent.putExtra(DBHelper.GENDER, gender);
+                intent.putExtra(DBHelper.DES, des);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Reload Data in listview
+        c = db.getAllUser();
+        adapter. changeCursor(c);
+        adapter.notifyDataSetChanged();
+        db.close();
+    }
+
+}
---
+++ SS9SQLLite_2/app/src/main/java/com/example/ss9sqllite_2/UpdateAct.java
@@ -0,0 +1,67 @@
+package com.example.ss9sqllite_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateAct extends AppCompatActivity implements View.OnClickListener {
+    private DBHelper db;
+    private int _id; private EditText edName;
+    private EditText edGender;
+    private EditText edDes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super. onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+        db = new DBHelper(this);
+        initView();
+        //Get Data From ListUserActivity
+        Intent intent = getIntent(); _id = intent.getIntExtra(DBHelper.ID, 0);
+        String name = intent.getStringExtra(DBHelper.NAME);
+        String gender = intent.getStringExtra(DBHelper.GENDER);
+        String des = intent.getStringExtra(DBHelper.DES);
+        edName.setText(name); edGender.setText(gender); edDes.setText(des);
+    }
+
+    private void initView() {
+        edName = (EditText) findViewById(R.id.edName);
+        edGender = (EditText) findViewById(R.id.edGender);
+        edDes = (EditText) findViewById(R.id.edDes);
+        Button btUpdate = (Button) findViewById(R.id.btUpdate);
+        btUpdate.setOnClickListener(this);
+        Button btDelete = (Button) findViewById(R.id.btDelete);
+        btDelete.setOnClickListener(this);
+    }
+
+    private void onUpdate() {
+        String isUpdate = db.updateUser(_id, edName.getText().toString(), edGender.getText().toString() + " update", edDes.getText().toString());
+        Toast.makeText(this, isUpdate, Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void onDelete() {
+        Toast.makeText(this, db.deleteUser(_id), Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btUpdate:
+                onUpdate();
+                break;
+            case R.id.btDelete:
+                onDelete();
+                break;
+            default:
+                break;
+        }
+    }
+
+}
